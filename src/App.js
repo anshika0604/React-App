4a814348c6f8b53db8ae2b9949cd44396ccdac48
Anshika <anshikanegi.cs19@gmail.com>
2023-05-09 20:34:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FunctionalComponent from './components/FunctionalComponent';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [name, setName] = useState("anshika");
   return (
-    <div className="App">
-      <h1>Class Component</h1>
-      <ClassComponent />
-      <h1>Functional Component</h1>
-      <FunctionalComponent name = "anshika" college = "deemed" age={21}/>
-    </div>
+    // <div className="App">
+    //   <h1>Class Component</h1>
+    //   <ClassComponent />
+    //   <h1>Functional Component</h1>
+    //   <FunctionalComponent name = {name} college = "deemed" age={21} setName = {setName}/>
+    // </div>
+    <Routes>
+      <Route path="/" element={<ClassComponent />}/>
+      <Route path="/functional-component" element={<FunctionalComponent 
+      name = {name} college = "deemed" age={21} setName = {setName}/>}/>
+    </Routes>
   );
 }
 
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,7 +1,9 @@
-import React ,{useState} from 'react';
+import React ,{ useState } from 'react';
 
 const FunctionalComponent = (props) => {
     const [count, setCount] = useState(0);
+    const [ changeName, setChangeName ] = useState("");
+    const {name, college, age, setName} = props;
     return (
         <div>
             <p>This is Functional Component</p>
@@ -9,7 +11,9 @@ const FunctionalComponent = (props) => {
             <button onClick={() => setCount(count+1)}>Click to increment</button>
             <button onClick={() => setCount(count-1)}>Click to decrement</button>
             <h2>{count}</h2>
-            <p>My name is {props.name} and my college is {props.college} and age is {props.age}</p>
+            <p>My name is {name} and my college is {college} and age is {age}</p>
+            <input onChange = {(e) => setChangeName(e.target.value)} />
+            <button onClick={() => setName(changeName)} > Click to change name </button>
         </div>
     );
 }
