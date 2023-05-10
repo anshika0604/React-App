c5983d37a3e9f8443ee095ac9e29ddae25c651bd
Anshika <anshikanegi.cs19@gmail.com>
2023-05-10 09:11:37 +0530

    updated

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class ClassComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("Constructor has been invoked");
+    }
+    componentDidMount() {
+        console.log("Component Did Mount Successfully");
+    }
+    componentWillUnmount() {
+        console.log("Component Unmounted Successfully");
+    }
     render() {
+        console.log("Render Method");
         return (
             <div>
                 <p>This is class Component</p>
